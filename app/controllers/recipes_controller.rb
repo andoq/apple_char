46b35dadadfccb8d93b6ec53a5798ab2d8665407
Andy Camp <andy@artefactgroup.com>
2014-05-25 11:13:06 -0700

    recipe added, and a few controllers

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,21 @@
+class RecipesController < ApplicationController
+  before_action :set_recipe, only: [:show]
+
+  def index
+    @recipes = Recipe.all
+  end
+
+  def show
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_recipe
+      @recipe = Recipe.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def recipe_params
+      params.require(:recipe).permit(:name, :method, :notes)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  resources :recipes, only: [:show, :index]
+
   devise_for :users
+
+  root 'recipes#index'
 end

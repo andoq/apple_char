5d3ff0840dc62b2db54f9a9ff4959e7c8ea74a8d
Andy Camp <andy@artefactgroup.com>
2014-05-25 16:18:52 -0700

    add basic model relationships, a render some stuff on the home page

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,7 +2,7 @@ class RecipesController < ApplicationController
   before_action :set_recipe, only: [:show]
 
   def index
-    @recipes = Recipe.all
+    @recipes = Recipe.all.limit(10)
   end
 
   def show
--- app/models/ingredient_recipe.rb
+++ app/models/ingredient_recipe.rb
@@ -1,2 +1,6 @@
 class IngredientRecipe < ActiveRecord::Base
+
+  belongs_to :ingredient, inverse_of: :ingredient_recipes
+  belongs_to :recipe, inverse_of: :ingredient_recipes
+
 end
